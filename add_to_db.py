ea5ffffb16f7e7d81cd3b5e3cd6327c5085278cb
schwarzschild-radius <pradeepisro49@gmail.com>
2019-11-19 16:17:13 +0530

    [SRC] Added script to write to mongodb

---
+++ add_to_db.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+from pymongo import MongoClient
+import logging
+from transform_json import transform_json
+from yaml2json import optYaml2Json
+
+def add_to_db(db, yaml_string):
+    json_string = optYaml2Json(yaml_string)
+    for each_pass in json_string:
+        db.analysis_results.insert_one(each_pass)
+
+if __name__ == "__main__":
+    import argparse
+    from pprint import pprint
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help='opt.yaml file to be converted')
+    parser.add_argument("--g", help="enable logging of debug info for developement only", action='store_true', default=False)
+    args = parser.parse_args()
+    if args.g:
+        logging.basicConfig(level=logging.DEBUG)
+    f = open(args.file)
+    g = f.read()
+    client = MongoClient('127.0.0.1:27017')
+    db = client.opt_remarks_analysis
+    server_status_result = db.command('serverStatus')
+    pprint(server_status_result)
+    add_to_db(db, g)
